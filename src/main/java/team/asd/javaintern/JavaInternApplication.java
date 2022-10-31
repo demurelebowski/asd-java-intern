996aa456cec077af6b6b6585d770b35570647712
Roman Demian <demurelebowski@gmail.com>
2022-10-31 15:37:32 +0200

    Added JavaInternApplication.java (test class @SpringBootApplication), added springframework dependencies in pom.xml

---
+++ src/main/java/team/asd/javaintern/JavaInternApplication.java
@@ -0,0 +1,13 @@
+package team.asd.javaintern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaInternApplication.class, args);
+	}
+
+}
