9b9e4cf1464c5833a80fc5c98aba7db296eab730
Roman Demian <demurelebowski@gmail.com>
2022-11-02 18:35:30 +0200

    #24 Refactor modules to team.asd.tutorials

---
+++ src/main/java/team/asd/tutorials/service/CalendarService.java
@@ -0,0 +1,103 @@
+package team.asd.tutorials.service;
+
+import org.apache.commons.lang3.ObjectUtils;
+import team.asd.tutorials.constants.DateElement;
+import team.asd.tutorials.exceptions.WrongArgumentException;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.WeekFields;
+import java.util.Locale;
+import java.util.Objects;
+
+public class CalendarService implements IsCalendarService {
+	final private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+	@Override
+	public String toString(LocalDate date) {
+		if (Objects.isNull(date)) {
+			return null;
+		}
+
+		return date.format(dateFormatter);
+	}
+
+	@Override
+	public LocalDate toLocalDate(String stringDate) {
+		if (Objects.isNull(stringDate)) {
+			return null;
+		}
+
+		try {
+			return LocalDate.parse(stringDate, dateFormatter);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	@Override
+	public long defineCountInRange(LocalDate fromDate, LocalDate toDate, ChronoUnit unit) {
+		if (ObjectUtils.anyNull(unit, toDate, fromDate)) {
+			throw new WrongArgumentException("Parameter is wrong");
+		}
+
+		try {
+			return unit.between(fromDate.atStartOfDay(), toDate.atStartOfDay());
+		} catch (Exception e) {
+			throw new WrongArgumentException("unit parameter is wrong");
+		}
+	}
+
+	@Override
+	public String getInfo(LocalDate date, DateElement dateElement) {
+		if (ObjectUtils.anyNull(dateElement, date)) {
+			throw new WrongArgumentException("Parameter is wrong");
+		}
+
+		switch (dateElement) {
+		case DAY_OF_WEEK:
+			return date.getDayOfWeek()
+					.toString();
+		case MONTH:
+			return date.getMonth()
+					.toString();
+		case WEEK_NUMBER:
+			return String.valueOf(date.get(WeekFields.of(Locale.getDefault())
+					.weekOfWeekBasedYear()));
+		case IS_LEAP_YEAR:
+			return date.isLeapYear() ? "Yes" : "No";
+		default:
+			throw new WrongArgumentException("Parameter is wrong");
+		}
+	}
+
+	@Override
+	public LocalDate reformatToLocalDate(String dateString) throws DateTimeException {
+		if (Objects.isNull(dateString)) {
+			throw new DateTimeException("Wrong parameter");
+		}
+		String[] arrDate = dateString.split(" ");
+
+		if (arrDate.length != 3) {
+			throw new DateTimeException("Wrong parameter");
+		}
+
+		try {
+			arrDate[0] = arrDate[0].replaceAll("[^\\d.]", "");
+			String stringDate = arrDate[0] + arrDate[1] + arrDate[2];
+			int year = Integer.parseInt(arrDate[2]);
+
+			if (year < 1000 || year > 3000) {
+				throw new DateTimeException("Wrong parameter");
+			}
+
+			return LocalDate.parse(stringDate, DateTimeFormatter.ofPattern("dMMMyyyy")
+					.withLocale(Locale.ENGLISH));
+
+		} catch (Exception e) {
+			throw new DateTimeException("Wrong parameter");
+		}
+	}
+}
---
+++ src/test/java/team/asd/tutorials/util/MessageBuilderTest.java
@@ -0,0 +1,57 @@
+package team.asd.tutorials.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import team.asd.tutorials.entities.BaseLocation;
+import team.asd.tutorials.entities.IsLocation;
+import team.asd.tutorials.entities.TestLocation;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageBuilderTest {
+
+	@Test
+	void add() {
+		Assertions.assertEquals("fieldTest: 123", new MessageBuilder().add(123, "fieldTest")
+				.getMessage());
+		assertEquals("123", new MessageBuilder().add(123, null)
+				.getMessage());
+	}
+
+	@Test
+	void testAddWithNull() {
+		MessageBuilder messageBuilder = new MessageBuilder().add(null, null);
+		assertNotNull(messageBuilder.getMessage(), "Should not be null");
+	}
+
+	@Test
+	void testAddLocation() {
+		IsLocation location = new TestLocation("Tschernikow", "Main", "12v");
+		String locationText = "Tschernikow Main, 12v";
+		MessageBuilder message = new MessageBuilder().add(location);
+		assertEquals(locationText, message.getMessage());
+	}
+
+	@Test
+	void testChangeBaseLocation() throws IllegalAccessException, InvocationTargetException {
+		BaseLocation baseLocation = new BaseLocation();
+		MessageBuilder messageBuilder = new MessageBuilder();
+		String changeTo = "Mars";
+
+		Method method = Arrays.stream(MessageBuilder.class.getDeclaredMethods())
+				.filter(met -> met.getName()
+						.equals("changeBaseLocation"))
+				.findFirst()
+				.orElseThrow(NoSuchElementException::new);
+
+		method.setAccessible(true);
+		method.invoke(messageBuilder, baseLocation, changeTo);
+
+		assertEquals(changeTo, baseLocation.getBaseLocation());
+	}
+}
