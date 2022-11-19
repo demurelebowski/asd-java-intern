004929165db17c71e1fd2eb0bf89103ffef3503a
Roman Demian <demurelebowski@gmail.com>
2022-11-19 13:30:28 +0200

    Added ArchivePriceTypeHandler

--- src/main/java/team/asd/constant/ArchivePriceType.java
+++ src/main/java/team/asd/constant/ArchivePriceType.java
@@ -1,5 +1,24 @@
 package team.asd.constant;
 
 public enum ArchivePriceType {
-	TAX, FEE, PRICE, INFO, COMMISSION
+	Tax("TAX"), Fee("FEE"), Price("PRICE"), Info("INFO"), Commission("COMMISSION");
+	private String value;
+
+	ArchivePriceType(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public static ArchivePriceType getByString(String str) {
+		for (ArchivePriceType archivePriceType : ArchivePriceType.values()) {
+			if (archivePriceType.getValue()
+					.equals(str)) {
+				return archivePriceType;
+			}
+		}
+		return null;
+	}
 }
