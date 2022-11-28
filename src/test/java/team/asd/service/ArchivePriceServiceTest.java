1c08e0f4198fa5606166c99be11e443610c05b2d
Roman Demian <demurelebowski@gmail.com>
2022-11-28 10:57:20 +0200

    Added Mockito dependency, added more tests

--- src/test/java/team/asd/service/ArchivePriceServiceTest.java
+++ src/test/java/team/asd/service/ArchivePriceServiceTest.java
@@ -1,39 +1,59 @@
 package team.asd.service;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import team.asd.constant.ArchivePriceState;
+import team.asd.constant.ArchivePriceType;
+import team.asd.constant.EntityType;
 import team.asd.dao.ArchivePriceDaoDummy;
 import team.asd.entity.ArchivePrice;
 import team.asd.exceptions.ValidationException;
 
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @DisplayName("Test methods for ArchivePriceService")
+@ExtendWith(MockitoExtension.class)
 class ArchivePriceServiceTest {
-	private static ArchivePriceService archivePriceService;
-	private ArchivePrice archivePrice;
 
-	@BeforeAll
-	public static void setUpClass() {
-		archivePriceService = new ArchivePriceService(new ArchivePriceDaoDummy());
-	}
+	private ArchivePriceService archivePriceService;
+
+	private ArchivePrice archivePrice;
+	@Mock
+	private ArchivePriceDaoDummy archivePriceDaoDummy;
 
 	@BeforeEach
 	public void initArchivePrice() {
+		archivePriceService = new ArchivePriceService(archivePriceDaoDummy);
 		archivePrice = new ArchivePrice();
-		archivePrice.setEntityId(8);
+		archivePrice.setId(-1);
 	}
 
 	@Test
-	void testReadById() {
+	void testReadByIdNotNull() {
+		when(archivePriceService.readById(1)).thenReturn(
+				new ArchivePrice(1, EntityType.Payment, 4, "Mok", ArchivePriceState.Created, ArchivePriceType.Price, 9.9, new Date()));
+		ArchivePrice archivePriceMok = archivePriceService.readById(1);
+		Assertions.assertNotNull(archivePriceMok);
+		Assertions.assertEquals(archivePriceMok.getName(), "Mok");
+		Assertions.assertEquals(archivePriceMok.getValue(), 9.9);
+	}
+
+	@Test
+	void testReadByIdInvalid() {
 		assertThrows(ValidationException.class, () -> archivePriceService.readById(null));
 		assertThrows(ValidationException.class, () -> archivePriceService.readById(-3));
 	}
 
 	@Test
-	void testCreateProduct() {
+	void testCreate() {
 		assertThrows(ValidationException.class, () -> archivePriceService.create(null));
 		assertThrows(ValidationException.class, () -> archivePriceService.create(new ArchivePrice()));
 		assertThrows(ValidationException.class, () -> archivePriceService.create(archivePrice));
@@ -44,6 +64,14 @@ class ArchivePriceServiceTest {
 		assertThrows(ValidationException.class, () -> archivePriceService.update(null));
 		assertThrows(ValidationException.class, () -> archivePriceService.update(new ArchivePrice()));
 		assertThrows(ValidationException.class, () -> archivePriceService.update(archivePrice));
+	}
+
+	@Test
+	void deleteValid() {
+		when(archivePriceService.delete(11)).thenReturn(true);
+		Boolean deleteSuccess = archivePriceService.delete(11);
+		Assertions.assertNotNull(deleteSuccess);
+		Assertions.assertEquals(deleteSuccess, true);
 	}
 
 	@Test
