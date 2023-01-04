cd6f7c15fb7288b3aba85256da0c2562a1875085
demure <demurelebowski@gmail.com>
2023-01-04 15:31:27 +0200

    Implemented readByKey

--- redis-layer/src/main/java/team/asd/dao/RedisClientDaoImpl.java
+++ redis-layer/src/main/java/team/asd/dao/RedisClientDaoImpl.java
@@ -1,11 +1,21 @@
 package team.asd.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
+import redis.clients.jedis.JedisPooled;
 
 @Repository
-public class RedisClientDaoImpl implements RedisClientDao{
+public class RedisClientDaoImpl implements RedisClientDao {
+	private @Value("${redis.host}") String redisHost;
+	private @Value("${redis.port}") int redisPort;
+	private @Value("${redis.password}") String password;
+
+	@Autowired
+	JedisPooled jedisPooled;
+
 	@Override
 	public String readByKey(String key) {
-		return "test";
+		return jedisPooled.get(key);
 	}
 }
